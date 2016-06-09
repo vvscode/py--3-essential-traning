c82fd3397b92bf0361601f166b887f0b8f6b253c
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-06-09 22:54:57 +0300

    Strings

--- 05/main.py
+++ 05/main.py
@@ -7,6 +7,26 @@ def main():
     print(num / 2, type(num / 2))
     print(float(5), type(float(5)))
 
+    s = r'This is \n string'
+    print(s)
+    s2 = 'This is \nstring'
+    print(s2)
+    # new style
+    print('This is {} forty two'.format(42))
+    # old style:
+    x = 23
+    print("this is a twenty three %s number" % x)
+    print('''
+    Test
+    ''')
+    # or
+    m_string = '''
+        here
+        multiple
+        rows
+    '''
+    print(m_string)
+
 
 if __name__ == '__main__':  # some comments in source code
     main()
