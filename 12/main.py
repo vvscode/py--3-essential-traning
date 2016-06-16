d1b218fe7f06037866a03f86d859db992b952b50
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-06-16 23:14:53 +0300

    Simple class inheritance

--- 12/main.py
+++ 12/main.py
@@ -1,20 +1,51 @@
 #!/usr/local/bin/python3
 
 
-class Duck:
-    def quack(self):
-        print('Duck->quack()')
+class Animal:
+    def talk(self):
+        print('Animal -> talk()')
 
     def walk(self):
-        print('Duck->walk()')
+        print('Animal -> walk()')
+
+    def clothes(self):
+        print('Animal -> clothes')
+
+
+class Duck(Animal):
+    def __init__(self, name=''):
+        # __ makes member private
+        self.__name = name
+
+    def quack(self):
+        print('Duck "{}" -> quack()'.format(self.__name))
+
+    def walk(self):
+        super().walk()
+        print('Duck "{}" -> walk()'.format(self.__name))
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+
+class Dog(Animal):
+    pass
 
 
 # classes
 def main():
-    donald = Duck()
+    donald = Duck('Donald')
     print(donald)
+    donald.set_name('DonDonald')
     donald.quack()
     donald.walk()
+    donald.talk()
+
+    fido = Dog()
+    fido.walk()
 
 # classes
 if __name__ == '__main__':  # some comments in source code
