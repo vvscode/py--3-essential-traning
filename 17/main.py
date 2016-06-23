2a07c4a07f4974a1cae3ccfa1515d3c550cdaccb
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-06-23 22:39:35 +0300

    Using modules

---
+++ 17/main.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+import os
+import sys
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+
+# modules
+def main():
+    print('Python version {}.{}.{}'.format(*sys.version_info))
+    print(sys.platform)
+    print()
+
+    import os
+    print(os.name)
+    print(os.getenv('PATH'))
+    print(os.getcwd())
+
+    # import urllib.request
+    # page = urllib.request.urlopen('http://app.collectriumdev.com')
+    # for line in page:
+    #     print(str(line, encoding='utf_8'), end='')
+    # print()
+
+    import datetime
+    now = datetime.datetime.now()
+    print(now)
+    print(now.year, now.month, now.day)
+    print()
+
+    # run `pip3 install bitstring` in cmd
+    import bitstring
+    a = bitstring.BitString(bin='01010101')
+    print(a.hex, a.bin, a.uint)
+
+
+if __name__ == '__main__':  # some comments in source code
+    main()
